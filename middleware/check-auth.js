080c94529e155f577e5d7f9dd2008abd182ea24d
Khaliddxx <doodz@aucegypt.edu>
2022-09-12 01:34:24 +0300

    heroku deployment test

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,8 +1,9 @@
 const HttpError = require("./http-error");
 const jwt = require("jsonwebtoken");
 
+// magic
 module.exports = (req, res, next) => {
-  if (req.method === 'OPTIONS') {
+  if (req.method === "OPTIONS") {
     return next();
   }
   try {
@@ -10,11 +11,11 @@ module.exports = (req, res, next) => {
     if (!token) {
       throw new Error("Authentication failed!");
     }
-    const decodedToken = jwt.verify(token, 'ia_65412654_ajbsc7qwe_ds');
+    const decodedToken = jwt.verify(token, "ia_65412654_ajbsc7qwe_ds");
     req.userData = { userId: decodedToken.userId };
     next();
   } catch (err) {
     const error = new HttpError("Authentication failed!", 401);
     return next(error);
   }
-}; 
+};
